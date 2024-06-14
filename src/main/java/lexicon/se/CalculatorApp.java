900568b85fe03ed8b25ce30856d64782fcc55e65
ryan.Rah <re.rahdari@gmail.com>
2024-06-14 14:43:00 +0200

    SImple Calculator App in Maven framework.

--- src/main/java/lexicon/se/CalculatorApp.java
+++ src/main/java/lexicon/se/CalculatorApp.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class CalculatorApp {
     public static void main(String[] args) {
 
+
         // Declare variables
         int operator;
         double number1, number2;
